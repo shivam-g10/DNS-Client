9a931620d4f37dc59523596d5588e83bbd013933
Shivam Mathur <shivam.mathurg10@gmail.com>
2019-02-15 09:33:05 +0530

    custom dns server

--- test/test.js
+++ test/test.js
@@ -1,10 +1,20 @@
 const Client = require("../index.js");
 let client = new Client();
-client.setUpServers(_=>{
+let round = 0;
+client.setUpServers("1.1.1.1",_=>{
     console.log(client)
     client.on("result",function(data){
         console.log(data)
-        client.close();
+        client.close(_=>{
+            if(round==0)
+            {
+                client.setUpServers(_=>{
+                    console.log(client)
+                    client.resolve("google.com","A");
+                })
+            }
+            round++;
+        });
     });
     client.resolve("google.com","A");
 })
